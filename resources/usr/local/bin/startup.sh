fe29f6dd30a1312b994a2218eec5fc1999da6b5e
William Miceli <1-WilliamMiceli@git.williammiceli.systems>
2019-03-30 16:57:15 -0400

    Removed unneeded functions

--- resources/usr/local/bin/startup.sh
+++ resources/usr/local/bin/startup.sh
@@ -2,45 +2,12 @@
 
 set -e
 
-function configure_admin() {
-    export GRAV_HOME=/var/www/grav-admin
-
-    # Setup admin user (if supplied)
-    if [ -z $ADMIN_USER ]; then
-        echo "[ INFO ] No Grav admin user details supplied"
-    else
-        if [ -e $GRAV_HOME/user/accounts/$ADMIN_USER.yaml ]; then
-            echo "[ INFO ] Grav admin user already exists"
-        else
-            echo "[ INFO ] Setting up Grav admin user"
-            cd $GRAV_HOME
-
-            sudo -u www-data bin/plugin login newuser \
-                 --user=${ADMIN_USER} \
-                 --password=${ADMIN_PASSWORD-"Pa55word"} \
-                 --permissions=${ADMIN_PERMISSIONS-"b"} \
-                 --email=${ADMIN_EMAIL-"admin@domain.com"} \
-                 --fullname=${ADMIN_FULLNAME-"Administrator"} \
-                 --title=${ADMIN_TITLE-"SiteAdministrator"}
-        fi
-    fi
-}
-
-function configure_nginx() {
-    echo "[ INFO ] Configuring Nginx"
-
-    echo "[ INFO ]  > Updating to listen on port 80"
-    sed -i 's/#listen 80;/listen 80;/g' /etc/nginx/conf.d/default.conf
-}
-
 function start_services() {
     echo "[ INFO ] Starting nginx"
     bash -c 'php5-fpm -D; nginx -g "daemon off;"'
 }
 
 function main() {
-    configure_admin
-    configure_nginx
     start_services
 }
 
