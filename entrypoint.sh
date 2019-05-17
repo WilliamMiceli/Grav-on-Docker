0b6ddce46dd3bc68dd23b4c75ac3489ca85ed1ca
William Miceli <1-WilliamMiceli@git.williammiceli.systems>
2019-05-16 23:36:10 -0400

    Temporarily disabled for testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 echo "[ INFO ] Starting PHP-FPM"
 php-fpm7 -D # Background
 
-echo "[ INFO ] Starting cron"
-crond # Background
+#echo "[ INFO ] Starting cron"
+#crond # Background
 
 echo "[ INFO ] Starting nginx"
 nginx -g "daemon off;" # Foreground
